f739e461ad4802d9d2ea77067c19ff5ecb604d48
Batrako <fsmetar@gmail.com>
2016-03-19 12:21:33 +0000

    presentacion con variables

--- app_server/controllers/villages.js
+++ app_server/controllers/villages.js
@@ -1,6 +1,57 @@
 /* GET 'homelist' page */
 module.exports.homelist = function(req, res){
-res.render('index', { title: 'Lista de Aldeas' });
+res.render('index', { 
+    title: 'Lista de Aldeas',
+    dataVillages: [{
+        Aldea: "Batrako",
+        Oro: 8000000,
+        PctOro: 93,
+        Elixir: 7000000,
+        PctElixir:85, 
+        Oscuro: 90000,
+        PctOscuro: 60, 
+        Constructores: 0,
+        Copas: 860,
+        Gemas: 980, 
+        Status: 1
+    },{
+        Aldea: "JaggerM3ister",
+        Oro: 8000000,
+        PctOro: 93,
+        Elixir: 7000000,
+        PctElixir:85, 
+        Oscuro: 90000,
+        PctOscuro: 60, 
+        Constructores: 0,
+        Copas: 860,
+        Gemas: 980, 
+        Status: 0
+    },{
+        Aldea: "Sus",
+        Oro: 8000000,
+        PctOro: 93,
+        Elixir: 7000000,
+        PctElixir:85, 
+        Oscuro: 90000,
+        PctOscuro: 60, 
+        Constructores: 0,
+        Copas: 860,
+        Gemas: 980, 
+        Status: 1
+    },{
+        Aldea: "Erik",
+        Oro: 8000000,
+        PctOro: 93,
+        Elixir: 7000000,
+        PctElixir:85, 
+        Oscuro: 90000,
+        PctOscuro: 60, 
+        Constructores: 0,
+        Copas: 860,
+        Gemas: 980, 
+        Status: 2
+    }]
+});
 };
 
 /* GET 'homelist' page */
